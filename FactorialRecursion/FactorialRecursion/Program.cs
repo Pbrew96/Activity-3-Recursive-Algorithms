fed383a1ad63b18abf1829b7034755bcc6acbcb8
Patrick Brewster <Patrick.shepherd33@gmail.com>
2026-07-04 09:09:28 -0400

    Add logic to solve the factorial problem using recursion

--- FactorialRecursion/FactorialRecursion/Program.cs
+++ FactorialRecursion/FactorialRecursion/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ * Patrick Brewster
+ * CST-250
+ * 07/03/2026
+ * Factorial Recursion
+ * Activity 3
+ */
+
--- FactorialRecursion/FactorialRecursion/Services/BusinessLogicLayer/FactorialLogic.cs
+++ FactorialRecursion/FactorialRecursion/Services/BusinessLogicLayer/FactorialLogic.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿/*
+ * Patrick Brewster
+ * CST-250
+ * 07/03/2026
+ * Factorial Recursion
+ * Activity 3
+ */
+
+
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace FactorialRecursion.Services.BusinessLogicLayer
 {
-    internal class FactorialLogic
+    public class FactorialLogic
     {
+
+        /// <summary>
+        ///  Solve the factorial problem using iteration
+        /// </summary>
+        /// <param name="factorial"></param>
+        /// <returns></returns>
+        internal BigInteger SolveIterativeFactorial(int factorial)
+        {
+            //Declare and initialize
+            BigInteger result = 1;
+
+            // For loop to solve the factorial
+            for(int i = factorial; i >= 1; i--)
+            {
+                // Multiply result by i
+                result *= i;
+            }
+            // Return the result
+            return result;
+        }
+        internal BigInteger SolveRecursiveFactorial(int factorial)
+        {
+            // Base Case: factorial is 0 or 1
+            if (factorial == 0 || factorial == 1)
+            {
+                return 1;
+            }
+            // Perform the recursion
+            return factorial * SolveRecursiveFactorial(factorial - 1);
+
+        }
     }
 }
